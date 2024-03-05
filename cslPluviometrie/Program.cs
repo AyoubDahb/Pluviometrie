bf6208f8adfe17d9b7b197548fa4485d6337488c
dahbi_a <dahbi_a@D202-022024-01>
2024-03-05 08:08:04 +0100

    Ajoutez des fichiers projet.

---
+++ cslPluviometrie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using libPluviometrie;
+
+int[] dijon = { 35, 38, 40, 24, 18, 11, 5, 4, 12, 19, 21, 29 };
+int[] beaune = { 31, 35, 38, 25, 19, 10, 6, 5, 13, 17, 20, 27 };
+int[] montbard = { 36, 36, 39, 23, 15, 9, 4, 3, 14, 20, 22, 28 };
+
+Pluviometrie cotedor = new Pluviometrie(dijon, beaune, montbard);
+
+Console.WriteLine("1- Voulez vous connaitre la pluviosité par mois ?");
+Console.WriteLine("2- Voulez vous connaitre la pluviosité par région ?");
+Console.WriteLine("3- Voulez vous connaitre la pluviosité par annuelle ?");
+
+string rep = Console.ReadLine();
+
+
+switch (rep)
+{
+    case ("1"):
+        cotedor.PluviositeMois();
+        break;
+    case ("2"):
+        cotedor.PluviositeRegion();
+        break;
+    case ("3"):
+        cotedor.PluviositeAnnee();
+        break;
+  
+}
---
+++ libPluviometrie/libPluviometrie.cs
@@ -0,0 +1,38 @@
+﻿namespace libPluviometrie
+{
+    public class Pluviometrie
+    {
+
+        private int[] ville1;
+        private int[] ville2;
+        private int[] ville3;
+
+        public Pluviometrie(int[] tab1, int[] tab2, int[] tab3)
+        {
+            this.ville1 = tab1;
+            this.ville2 = tab2;
+            this.ville3 = tab3;
+        }
+
+        public string Affiche(int[] tab)
+        {
+            return "";
+        }
+
+        public int PluviositeAnnee()
+        {
+            return 0;
+        }
+
+        public int[] PluviositeMois()
+        {
+            return ville1;
+        }
+
+        public int PluviositeRegion()
+        {
+            return 0;
+        }
+        
+    }
+}
